77e882336e59a24ed542be263b1d44d6139755e4
bondanjody <bondanjodys@gmail.com>
2023-02-28 13:19:40 +0700

    Menambahkan file concatVspush.js. Membandingkan method concat dan push pada array di JavaScript.

---
+++ concatVspush.js
@@ -0,0 +1,10 @@
+let arr1 = [
+    "London",
+    "Berlin",
+    "Rome"
+];
+
+const arr1Concat = arr1.concat("Madrid");
+const arr1Push = arr1.push("Nairobi");
+console.log(arr1Concat);
+console.log(arr1Push);
